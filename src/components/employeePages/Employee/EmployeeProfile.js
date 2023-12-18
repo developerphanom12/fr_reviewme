523479d1d340f247805b7ce77b459374f3366923
developer <developer.phanomprofessionals@gmail.com>
2023-12-18 19:05:02 +0530

    profile education and about edit

--- src/components/employeePages/Employee/EmployeeProfile.js
+++ src/components/employeePages/Employee/EmployeeProfile.js
@@ -10,43 +10,76 @@ export default function EmployeeProfile() {
   const navigate = useNavigate();
   return (
     <div className="root_div">
-      <div className="main1">
-        <div className="profile">
-          <div className="bg_img">
-            <img src={backimg} alt="img" />
+      <div className="first_main_div">
+        <div className="main1">
+          <div className="profile">
+            <div className="bg_img">
+              <img src={backimg} alt="img" />
 
-            <IoCameraOutline />
+              <IoCameraOutline />
+            </div>
+            <div className="imgg">
+              <img src={background} alt="img" />{" "}
+            </div>
+            <div className="bio">
+              <div className="bio_btn_div">
+                <button
+                  className="bio_button"
+                  onClick={() => {
+                    navigate("/employeeview");
+                  }}
+                >
+                  View details
+                </button>
+                <button
+                  onClick={() => {
+                    navigate("/editintro");
+                  }}
+                >
+                  <CiEdit />
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="imgg">
-            <img src={background} alt="img" />{" "}
-          </div>
-          <div className="bio">
-            <div className="bio_btn_div">
-              <button
-                className="bio_button"
-                onClick={() => {
-                  navigate("/employeeview");
-                }}
-              >
-                View details
-              </button>
-              <button
-                onClick={() => {
-                  navigate("/editintro");
-                }}
-              >
-                <CiEdit />
-              </button>
+          <div className="bio_2">
+            <div className="bio_name">
+              <h2>Srishti</h2>
+            </div>
+            <div className="connection">
+              {" "}
+              Founder & CEO at Phanom Professionals{" "}
+            </div>
+            <div className="items">
+              {" "}
+              <button>Follow</button> <button>Message</button>{" "}
+              <button>More</button>
             </div>
           </div>
         </div>
-        <div className="bio_2">
-          <div className="bio_name"><h2>Srishti</h2></div>
-          <div className="connection"> connection </div>
-          <div className="items"> my items </div>
+        <div className="about">
+          <h1>About</h1>
         </div>
       </div>
-      <div className="main2">suggestions</div>
+
+      <div className="main2">
+        <div className="main2_child">
+          <h6>Ad...</h6>
+          <p>company image</p>
+          <button>Follow</button>
+        </div>
+        <div className="main2_child">
+          <div className="main2_child2">
+            <h6> People also viewed</h6>
+            <p>Profile image | person name</p>
+            <button>+ Follow</button>
+          </div>
+          <div className="main2_child2">
+            <h6> People also viewed</h6>
+            <p>Profile image | person name</p>
+            <button>+ Follow</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
